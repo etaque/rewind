5a23c7e0fa09073484350a1a8de73aac2bb0da97
Emilien Taque <emilien@dividat.ch>
2020-11-24 14:25:21 +0100

    Introduce Elm

--- shell.nix
+++ shell.nix
@@ -1,13 +1,27 @@
-with (import <nixpkgs> {});
+with import (builtins.fetchGit {
+  name = "nixpkgs-20.09";
+  url = "git@github.com:nixos/nixpkgs.git";
+  rev = "cd63096d6d887d689543a0b97743d28995bc9bc3";
+  ref = "refs/tags/20.09";
+}) {};
 
 mkShell {
 
   buildInputs = [
+    # backend
     sbt
     flyway
     ansible
     postgresql_11
     postgis
+
+    # frontend
+    nodejs
+    elmPackages.elm
+    elmPackages.elm-format
+    elmPackages.elm-test
+    inotify-tools
   ];
 
 }
+
