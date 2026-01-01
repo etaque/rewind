f0d2e4ba33677e5ca82b17c79a35c5dec44540d0
Emilien Taque <e@taque.fr>
2026-01-01 08:49:02 +0100

    Switch to docker, get rid of nix-containers

--- config/setup-db.sql
+++ config/setup-db.sql
@@ -1,4 +1,7 @@
--- @see docker-compose.yml
+-- Enable required PostGIS extensions
+CREATE EXTENSION IF NOT EXISTS hstore;
+CREATE EXTENSION IF NOT EXISTS postgis_raster;
 
+-- Enable all GDAL drivers for raster operations
 ALTER DATABASE rewind SET postgis.gdal_enabled_drivers TO 'ENABLE_ALL';
 SELECT pg_reload_conf();
