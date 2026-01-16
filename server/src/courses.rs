b52f1f9c1af8aa85a9045e027927007ed373d67d
Emilien Taque <e@taque.fr>
2026-01-16 09:22:58 +0100

    add route du rhum 2022

--- server/src/courses.rs
+++ server/src/courses.rs
@@ -20,21 +20,40 @@ pub struct Course {
 }
 
 pub fn all() -> Vec<Course> {
-    vec![Course {
-        key: "vg20".to_string(),
-        name: "Vendee Globe 2020".to_string(),
-        // 2020-11-08T11:00:00+01:00 in milliseconds
-        start_time: 1604833200000,
-        start: LngLat {
-            lng: -1.788456535301071,
-            lat: 46.470243284275966,
+    vec![
+        Course {
+            key: "vg20".to_string(),
+            name: "Vendee Globe 2020".to_string(),
+            // 2020-11-08T11:00:00+01:00 in milliseconds
+            start_time: 1604833200000,
+            start: LngLat {
+                lng: -1.788456535301071,
+                lat: 46.470243284275966,
+            },
+            start_heading: 270.0,
+            finish: LngLat {
+                lng: -1.788456535301071,
+                lat: 46.470243284275966,
+            },
+            time_factor: 2000,
+            max_days: 90,
         },
-        start_heading: 270.0,
-        finish: LngLat {
-            lng: -1.788456535301071,
-            lat: 46.470243284275966,
+        Course {
+            key: "rdr22".to_string(),
+            name: "Route du Rhum 2022".to_string(),
+            // 2022-11-09T13:15:00Z in milliseconds
+            start_time: 1668002100000,
+            start: LngLat {
+                lng: -2.0,
+                lat: 48.65,
+            },
+            start_heading: 225.0,
+            finish: LngLat {
+                lng: -61.53,
+                lat: 16.23,
+            },
+            time_factor: 180,
+            max_days: 21,
         },
-        time_factor: 2000,
-        max_days: 90,
-    }]
+    ]
 }
